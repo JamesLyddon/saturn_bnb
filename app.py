1b975fcda33f52c402abbece10d5cede81c057e0
james lyddon <jameslyddon@gmail.com>
2025-07-21 21:22:09 +0100

    added login functionality

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from lib.dummy_user_repo import DummyRepo
 from flask_bcrypt import Bcrypt
 from lib.forms.register_form import RegisterForm
 from lib.forms.login_form import LoginForm
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
+
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -16,6 +18,18 @@ bycrpt = Bcrypt(app)
 
 # Example for development
 app.config['SECRET_KEY'] = 'supersecretkey'
+
+# Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    connection = get_flask_database_connection(app)
+    repo = DummyRepo(connection)
+    user = repo.find(user_id)
+    return user
 
 # check username exists
 def validate_username(username):
@@ -46,7 +60,28 @@ def get_index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    
+    if form.validate_on_submit():
+        connection = get_flask_database_connection(app)
+        repo = DummyRepo(connection)
+        users = repo.all()
+    
+        for user in users:
+            if user.username == form.username.data:
+                if bycrpt.check_password_hash(user.password, form.password.data):
+                    login_user(user)
+                    return redirect(url_for('dashboard'))
+    
+    
+    
+    
     return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+    
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -54,7 +89,8 @@ def register():
     
     if form.validate_on_submit():
         hashed_password = bycrpt.generate_password_hash(form.password.data)
-        new_user = Dummy(None, username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, password=hashed_password, email=form.email.data, phone_number=form.phone_number.data)
+        hashed_password_string = hashed_password.decode('utf-8')
+        new_user = Dummy(None, username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, password=hashed_password_string, email=form.email.data, phone_number=form.phone_number.data)
     
         connection = get_flask_database_connection(app)
         repo = DummyRepo(connection)
