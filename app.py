a35901145d84507febbe8d547e5d1de11f999a49
Liza Tarasova <tarasova.liza.vic@gmail.com>
2025-07-22 15:56:49 +0100

    TDD html signup page

--- app.py
+++ app.py
@@ -23,8 +23,24 @@ def get_all_spaces():
     spaces = repo.all()
     return render_template('spaces.html', spaces=spaces)
 
+@app.route('/register', methods=["GET"])
+def get_register():
+    return render_template('register.html')
+
+@app.route('/register', methods=["POST"])
+def post_register():
+    username = request.form.get('username')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    password = request.form.get('password')
+    email = request.form.get('email')
+    phone_number = request.form.get('phone_number')
+
+    return f"User succesfully registered!", 200
+
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,12 @@
 from playwright.sync_api import Page, expect
+import pytest
+from app import app
 
 # Tests for your routes go here
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    return app.test_client()
 
 """
 We can render the index page
@@ -14,3 +20,47 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(p_tag).to_have_text("This is the homepage.")
+
+def test_get_signup_page(page, test_web_address):
+    page.goto(f"http://{test_web_address}/register")
+
+    h2_tag = page.locator("h2")
+    expect(h2_tag).to_have_text("Register a new user")
+    
+    label_tag = page.locator("label").nth(0)
+    expect(label_tag).to_have_text("User name:")
+    
+    label_tag = page.locator("label").nth(1)
+    expect(label_tag).to_have_text("First name:")
+    
+    label_tag = page.locator("label").nth(2)
+    expect(label_tag).to_have_text("Last name:")
+    
+    label_tag = page.locator("label").nth(3)
+    expect(label_tag).to_have_text("Password:")
+    
+    label_tag = page.locator("label").nth(4)
+    expect(label_tag).to_have_text("Email:")
+    
+    label_tag = page.locator("label").nth(5)
+    expect(label_tag).to_have_text("Phone number:")
+
+    button_tag = page.locator("button")
+    expect(button_tag).to_have_text("Submit")
+
+def test_register_success(client):
+    response = client.post('/register', data={
+        'username': 'Saima1',
+        'first_name': 'Saima',
+        'last_name': 'Abdus',
+        'password': 'saima123',
+        'email': 'saima@email.com',
+        'phone_number': '01234567890',
+    })
+    assert response.status_code == 200
+    assert f"User succesfully registered!"
+
+
+
+# def test_post_new_user(page, test_web_address):
+#     page.goto(f"http://{test_web_address}/register")
