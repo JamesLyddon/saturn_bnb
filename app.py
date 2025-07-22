cff09b7d036fd9590a7ddfc79c468b7fae988772
james lyddon <jameslyddon@gmail.com>
2025-07-22 16:46:39 +0100

    refactored app.py

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 import os
+# ==== packages ====
 from flask import Flask, request, render_template, url_for, redirect
 from lib.database_connection import get_flask_database_connection
 from wtforms.validators import ValidationError
-from lib.dummy_user import Dummy
-from lib.dummy_user_repo import DummyRepo
 from flask_bcrypt import Bcrypt
 from lib.forms.register_form import RegisterForm
 from lib.forms.login_form import LoginForm
 from flask_login import login_user, LoginManager, login_required, logout_user, current_user
-
-from flask import Flask, request, render_template, redirect
-from lib.database_connection import get_flask_database_connection
+# ==== Models & Repos ====
 from lib.space_repository import SpaceRepository
 from lib.space import Space
+from lib.user_repository import UserRepository
+from lib.user import User
 
+# ==== Set up ====
 # Create a new Flask app
 app = Flask(__name__)
 
-# Password hashing
+# Password hashing using bycrpt
 bycrpt = Bcrypt(app)
 
-# Example for development
+# Example for development (can go through setting up a .env file together later)
 app.config['SECRET_KEY'] = 'supersecretkey'
 
-# Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -31,14 +30,14 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     connection = get_flask_database_connection(app)
-    repo = DummyRepo(connection)
+    repo = UserRepository(connection)
     user = repo.find(user_id)
     return user
 
 # check username exists
 def validate_username(username):
     connection = get_flask_database_connection(app)
-    repo = DummyRepo(connection)
+    repo = UserRepository(connection)
     users = repo.all()
     username_exists = False
     
@@ -50,24 +49,14 @@ def validate_username(username):
         raise ValidationError(
             'That username already exists. Please choose a different one.')
 
-
-# == Your Routes Here ==
-
-# GET /index
-# Returns the homepage
-# Try it:
-#   ; open http://localhost:5001/index
-@app.route('/index', methods=['GET'])
-def get_index():
-    return render_template('index.html')
-
+# ==== Register, Login, Logout Routes ====
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     
     if form.validate_on_submit():
         connection = get_flask_database_connection(app)
-        repo = DummyRepo(connection)
+        repo = UserRepository(connection)
         users = repo.all()
     
         for user in users:
@@ -89,7 +78,6 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -97,28 +85,26 @@ def register():
     if form.validate_on_submit():
         hashed_password = bycrpt.generate_password_hash(form.password.data)
         hashed_password_string = hashed_password.decode('utf-8')
-        new_user = Dummy(None, username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, password=hashed_password_string, email=form.email.data, phone_number=form.phone_number.data)
+        new_user = User(None, username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, password=hashed_password_string, email=form.email.data, phone_number=form.phone_number.data)
     
         connection = get_flask_database_connection(app)
-        repo = DummyRepo(connection)
+        repo = UserRepository(connection)
         
-        user = repo.create(new_user)
+        repo.create(new_user)
         
         return redirect(url_for('login'))
     
     return render_template('register.html', form=form)
 
+# to be deleted ❌
 @app.route('/users', methods=['GET'])
 def users():
     connection = get_flask_database_connection(app)
-    repo = DummyRepo(connection)
+    repo = UserRepository(connection)
     users = repo.all()
     return render_template('users.html', users=users)
     
-    
-    
-    
-    
+# ==== Spaces Routes ====
 @app.route('/spaces', methods=['GET'])
 def get_all_spaces():
     connection = get_flask_database_connection(app)
