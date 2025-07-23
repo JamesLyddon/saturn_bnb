d004e03835b2c694e0cdec7c45e5d78505267360
Sarmad Khatri <sarmadkhatri15@gmail.com>
2025-07-23 16:44:47 +0100

    attempting to add a new fixture for sign in

--- tests/test_new_space_route.py
+++ tests/test_new_space_route.py
@@ -2,11 +2,22 @@ from playwright.sync_api import Page, expect
 
 # Need a login step before /spaces/new can be accessed
 
-# def test_get_form(page, test_web_address):
-#     page.goto(f'http://{test_web_address}/spaces/new')
-#     list_labels = page.locator('label')
 
-#     expect(list_labels).to_have_text(["Title", "Description", "Price", "Address"])
+def sign_in_user(db_connection, test_web_address, page):
+    db_connection.seed('seeds/bnb_seed.sql')
+    page.goto(f'http://{test_web_address}/login')
+    page.fill('input[name=username]', 'janesmith')
+    page.fill('input[name=password]', 'SuperSecret999')
+    page.click('button[type=submit]')
+    headline = page.locator('.t-headline')
+    expect(headline).to_have_text('Available Spaces')
+    
+def test_get_form(page, test_web_address, sign_in_user, db_connection):
+    sign_in_user(db_connection, test_web_address, page)
+    # page.goto(f'http://{test_web_address}/spaces/new')
+    # list_labels = page.locator('label')
+
+    # expect(list_labels).to_have_text(["Title", "Description", "Price", "Address"])
 
 # def test_create_new_space(db_connection, page, test_web_address):
 #     db_connection.seed('seeds/bnb_seed.sql')
