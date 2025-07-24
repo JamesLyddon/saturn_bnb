bdb7fe188058580102d7fbe70f10fdcaca73462b
Sarmad Khatri <sarmadkhatri15@gmail.com>
2025-07-24 10:29:31 +0100

    fixture for sign in

--- tests/test_new_space_route.py
+++ tests/test_new_space_route.py
@@ -1,36 +1,26 @@
 from playwright.sync_api import Page, expect
 
 # Need a login step before /spaces/new can be accessed
-
-
-def sign_in_user(db_connection, test_web_address, page):
-    db_connection.seed('seeds/bnb_seed.sql')
-    page.goto(f'http://{test_web_address}/login')
-    page.fill('input[name=username]', 'janesmith')
-    page.fill('input[name=password]', 'SuperSecret999')
-    page.click('button[type=submit]')
-    headline = page.locator('.t-headline')
-    expect(headline).to_have_text('Available Spaces')
     
-def test_get_form(page, test_web_address, sign_in_user, db_connection):
+def test_get_form(page, test_web_address, sign_in_user):
     sign_in_user()
     expect(page.locator('.t-headline')).to_have_text('Available Spaces')
-    # page.goto(f'http://{test_web_address}/spaces/new')
-    # list_labels = page.locator('label')
+    page.goto(f'http://{test_web_address}/spaces/new')
+    list_labels = page.locator('label')
 
-    # expect(list_labels).to_have_text(["Title", "Description", "Price", "Address"])
+    expect(list_labels).to_have_text(["Title", "Description", "Price", "Address"])
 
-# def test_create_new_space(db_connection, page, test_web_address):
-#     db_connection.seed('seeds/bnb_seed.sql')
-#     page.goto(f'http://{test_web_address}/spaces/new')
+def test_create_new_space(db_connection, page, test_web_address, sign_in_user):
+    sign_in_user()
+    page.goto(f'http://{test_web_address}/spaces/new')
 
-#     page.fill("input[name=title]", 'a lovely house in newcastle')
-#     page.fill("input[name=description]", '5 bedroom house with pool')
-#     page.fill("input[name=price]", '100')
-#     page.fill("input[name=address]", '124 Oxford Street')
+    page.fill("input[name=title]", 'a lovely house in newcastle')
+    page.fill("input[name=description]", '5 bedroom house with pool')
+    page.fill("input[name=price]", '100')
+    page.fill("input[name=address]", '124 Oxford Street')
 
-#     page.click("text=Create Space")
+    page.click("text=Create Space")
 
-#     spaces_titles = page.locator(".t-space-title")
+    spaces_titles = page.locator(".t-space-title")
 
-#     expect(spaces_titles).to_have_text(["Cozy Apartment in Ce...", "Spacious Family Home...", 'Beachfront Villa wit...', 'Charming Edinburgh L...', 'Rustic Cottage in Sc...', 'a lovely house in ne...'])
+    expect(spaces_titles).to_have_text(["Cozy Apartment in Ce...", "Spacious Family Home...", 'Beachfront Villa wit...', 'Charming Edinburgh L...', 'Rustic Cottage in Sc...', 'a lovely house in ne...'])
