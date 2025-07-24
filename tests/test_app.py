377c10fb59bd5528fbac0b48f0c7c38164f60e5d
Saima Abdus <saima.abdus@kraken.tech>
2025-07-24 15:43:39 +0100

    Fixed sign-up page tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,49 +8,50 @@ def client():
     app.config["TESTING"] = True
     return app.test_client()
 
-# def test_get_signup_page(page, test_web_address):
-#     page.goto(f"http://{test_web_address}/register")
 
-#     h2_tag = page.locator("h2")
-#     expect(h2_tag).to_have_text("Register a new user")
-    
-#     label_tag = page.locator("label").nth(0)
-#     expect(label_tag).to_have_text("User name:")
-    
-#     label_tag = page.locator("label").nth(1)
-#     expect(label_tag).to_have_text("First name:")
-    
-#     label_tag = page.locator("label").nth(2)
-#     expect(label_tag).to_have_text("Last name:")
-    
-#     label_tag = page.locator("label").nth(3)
-#     expect(label_tag).to_have_text("Password:")
-    
-#     label_tag = page.locator("label").nth(4)
-#     expect(label_tag).to_have_text("Email:")
-    
-#     label_tag = page.locator("label").nth(5)
-#     expect(label_tag).to_have_text("Phone number:")
+"""
+Tests for new user sign up page
+"""
+def test_get_signup_page(page, test_web_address):
+    page.goto(f"http://{test_web_address}/register")
 
-#     button_tag = page.locator("button")
-#     expect(button_tag).to_have_text("Submit")
+    h1_tag = page.locator(".t-headline")
+    expect(h1_tag).to_have_text("Register")
 
-# def test_register_success(client):
-#     response = client.post('/register', data={
-#         'username': 'Saima1',
-#         'first_name': 'Saima',
-#         'last_name': 'Abdus',
-#         'password': 'saima123',
-#         'email': 'saima@email.com',
-#         'phone_number': '01234567890',
-#     })
-#     assert response.status_code == 200
-#     assert f"User succesfully registered!"
+    label_tag = page.locator("label.label").nth(0)
+    expect(label_tag).to_have_text("Username")
+    
+    label_tag = page.locator("label.label").nth(1)
+    expect(label_tag).to_have_text("First Name")
+    
+    label_tag = page.locator("label.label").nth(2)
+    expect(label_tag).to_have_text("Last Name")
+    
+    label_tag = page.locator("label.label").nth(3)
+    expect(label_tag).to_have_text("Password")
+    
+    label_tag = page.locator("label.label").nth(4)
+    expect(label_tag).to_have_text("Email")
+    
+    label_tag = page.locator("label.label").nth(5)
+    expect(label_tag).to_have_text("Phone Number")
+    
+    button_tag = page.locator("button")
+    expect(button_tag).to_have_text("Register")
 
 
+def test_user_registration_successful(client):
+    response = client.post('/register', data={
+        'username': 'Saima1',
+        'first_name': 'Saima',
+        'last_name': 'Abdus',
+        'password': 'saima123',
+        'email': 'saima@email.com',
+        'phone_number': '01234567890',
+    })
+    assert response.status_code == 200
+    assert f"User succesfully registered!"
 
-# def test_post_new_user(page, test_web_address):
-#     page.goto(f"http://{test_web_address}/register")
 
 
 """
